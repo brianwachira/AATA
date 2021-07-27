05a68f81f2a07be33a2b25b0fbde5eb319a81634
brianwachira <brianwachira7@gmail.com>
2021-07-28 00:59:48 +0300

    [FEATURE] : add types for clinic, staff, patient, guardian/ next of kin, finances, consultation and admin

--- backend/types.ts
+++ backend/types.ts
@@ -4,34 +4,89 @@ export enum Gender {
     Female = 'f',
 }
 
+interface timeStamps{
+    createdAt?: Date;
+    updatedAt?: Date;
+}
 interface BasePerson{
     firstName: string;
     lastName: string;
     DOB: Date;
     Gender: Gender;
-    createdAt: Date;
-    updatedAt: Date;
 }
 
+interface ContactInfo{
+    nationalID: string;
+    phoneNo: string;
+    email: string;
+
+}
+interface Credentials{
+    password: string;
+
+}
 interface objectReference{
     id: string;
 }
-export interface Unormal extends BasePerson{
+interface staffType{
+    staffType: string
+}
+export interface Unormal extends BasePerson, ContactInfo, Credentials, timeStamps{
     _id?: string;
     __v?: string;
     id?: string;
-    nationalID: string;
-    phoneNo: string;
-    email: string;
-    password: string;
     assessments? : objectReference[];
 }
 
-export interface dispatchOfficer extends BasePerson{
-    nationalID: string;
-    phoneNo: string;
-    email: string;
-    password: string;
+export interface admin extends BasePerson, ContactInfo, Credentials, timeStamps{
+    clinicsLaunched: objectReference[];
+}
+
+export interface dispatchOfficer extends BasePerson, ContactInfo, Credentials, timeStamps{
     assessments? : objectReference[];
 }
+
+export interface clinic extends timeStamps{
+    name: string;
+    location: string;
+    staff?: objectReference[];
+    createdBy: string;
+}
+
+export interface staff extends BasePerson,ContactInfo, staffType,timeStamps{
+    branch: objectReference[];
+    consultations: objectReference[];
+
+}
+
+export interface patient extends BasePerson, timeStamps{
+    consultations: objectReference[];
+}
+export interface guardianorkin extends BasePerson, ContactInfo, timeStamps{
+    patients: objectReference[];   
+}
+export interface diagnosis extends timeStamps {
+    code : string;
+    name? : string;
+    description : string;
+    remedy : string;
+}
+export interface consultation extends timeStamps {
+    patientID: string;
+    staffID: string;
+    branchID: string;
+    diagnosis: objectReference[];
+}
+export interface consultationRating extends timeStamps {
+    consultationID : string;
+    rating: number;
+    comment: string;
+}
+
+export interface payments extends timeStamps {
+    consultationID: string;
+    branchID: string;
+    amount: number;
+}
+
 
