c28ab5e2c30d67b9576338d9279396fc21918c1d
brianwachira <brianwachira7@gmail.com>
2021-07-28 21:19:22 +0300

    [FEATURE] : add model for clinic

---
+++ backend/models/Clinics.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+
+import { NewClinicEntry } from '../types';
+
+const clinicSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    createdBy: {
+        type: String,
+        required: true
+    },
+    staff:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Staff'
+        }
+    ]  
+},{timestamps: true});
+
+clinicSchema.plugin(uniqueValidator);
+
+// clinicSchema.methods.transform =  function(){
+//     const obj = this.toObject();
+//     const id : never = obj._id;
+//     delete obj._id;
+//     obj.id = id;
+    
+//     return obj;
+// };
+
+const Clinic = mongoose.model<NewClinicEntry>('Clinic', clinicSchema);
+
+module.exports = Clinic;
