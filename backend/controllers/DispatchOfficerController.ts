05c85a8d875823569ad87dc23a10865e5f83cfbd
brianwachira <brianwachira7@gmail.com>
2021-07-31 16:20:00 +0300

    [FEATURE] : add graphql model for dispatch officer

---
+++ backend/controllers/DispatchOfficerController.ts
@@ -0,0 +1,89 @@
+import { UserInputError } from "apollo-server";
+import DispatchOfficer from "../models/DipatchOfficers";
+import { dispatchOfficer, NewDispatchOfficerEntry } from "../types";
+import bcrypt from 'bcrypt';
+import { parseString } from "../typeGuards";
+//createDispatchOfficer, updateDispatchOfficer
+//getDispatchOfficer, getAllDispatchOfficers
+
+/**
+ * @description holds crud operations for the dispatch officer entity 
+ */
+
+/**
+ * creates dispatch officer
+ * @param args {NewDispatchOfficer} dispatch officer
+ * @returns {dispatchOfficer} created dispatch officer
+ */
+
+export const createDispatchOfficer = async (args: NewDispatchOfficerEntry): Promise<dispatchOfficer> => {
+    const saltRounds = 10;
+
+    /**
+     * hash password using bycrypt
+     * use typeguards to ensure return value is a string
+     **/
+    //eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    const passwordHash: string = parseString(await bcrypt.hash(args.password, saltRounds));
+    const dispatchOfficer = new DispatchOfficer({ ...args, password: passwordHash });
+    try {
+        await dispatchOfficer.save();
+    } catch (error) {
+        throw new UserInputError(error.message, {
+            invalidArgs: args,
+        });
+    }
+    return dispatchOfficer;
+};
+
+/**
+ * updates Dispatch Officer
+ * @param args dispatch officer
+ * @returns {dispatchOfficer} updated dispatch officer
+ */
+
+export const updateDispatchOfficer = async (args: dispatchOfficer): Promise<dispatchOfficer> => {
+    let dispatchOfficer = await DispatchOfficer.findOne({ email: args.email });
+
+    if (!dispatchOfficer) {
+        throw new UserInputError("Dispatch Officer account not available", { invalidArgs: args });
+    }
+    dispatchOfficer = args;
+
+    try {
+        await dispatchOfficer.save();
+    } catch (error) {
+        throw new UserInputError(error.message, { invalidArgs: args });
+    }
+
+    return dispatchOfficer;
+};
+
+/**
+ * get all dispatch officers
+ * @returns {dispatchOfficer[]} list all of dispatch officers
+ */
+
+export const getAllDispatchOfficers = async (): Promise<dispatchOfficer[]> => {
+    const dispatchOfficers: dispatchOfficer[] = await DispatchOfficer.find();
+
+    return dispatchOfficers;
+};
+
+/**
+ * gets dispatch officer
+ * @param args id
+ * @returns {dispatchOfficer} dispatch officer
+ */
+
+export const getDispatchOfficer = async (args: { id: string; }): Promise<dispatchOfficer> => {
+    const dispatchOfficer = await DispatchOfficer.findById(args);
+
+
+
+    if (!dispatchOfficer) {
+        throw new UserInputError("User is not available", { invalidArgs: args.id });
+    }
+
+    return dispatchOfficer;
+};
---
+++ backend/queries/DispatchOfficerQuery.ts
@@ -0,0 +1,20 @@
+import { getDispatchOfficer, getAllDispatchOfficers} from '../controllers/DispatchOfficerController';
+import { dispatchOfficer } from '../types';
+
+/**
+ * @description holds Dispatch Officer queries
+ */
+
+export const DispatchOfficerQuery = {
+    dispatchOfficers : {
+        resolve: async()=>{
+            return await getAllDispatchOfficers();
+        },
+    },
+    dispatchOfficer: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        resolve: async(_parent:any,args:dispatchOfficer, _context: any, _info: any) => {
+            return await getDispatchOfficer(args.id);
+        },
+    }
+};
