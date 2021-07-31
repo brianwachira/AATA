5c9cbde2ba2449c63a3d986ffa5f8548fc949be4
brianwachira <brianwachira7@gmail.com>
2021-08-01 01:19:05 +0300

    [FEATURE] : add patient schema and resolver to schema and resolver

--- backend/resolvers/index.ts
+++ backend/resolvers/index.ts
@@ -1,10 +1,11 @@
 import { AdminResolver } from "./AdminResolver";
 import { ClinicResolver } from "./ClinicResolver";
 import { DispatchOfficerResolver } from "./DispatchOfficerResolver";
+import { PatientResolver } from "./PatientResolver";
 import { StaffResolver } from "./StaffResolver";
 
 /**
  * @description holds all resolvers
  */
 
-export const resolvers = [AdminResolver, DispatchOfficerResolver, StaffResolver, ClinicResolver];
+export const resolvers = [AdminResolver, DispatchOfficerResolver, StaffResolver, ClinicResolver, PatientResolver];
