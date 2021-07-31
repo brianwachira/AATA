dce4c64943d0fdc46d27e85a62d4efe0543a5f26
brianwachira <brianwachira7@gmail.com>
2021-07-31 13:17:01 +0300

    [FEATURE] : add controllers that holds all crud operations for admin

---
+++ backend/controllers/AdminController.ts
@@ -0,0 +1,90 @@
+import { UserInputError } from "apollo-server";
+import Admin from "../models/Admins";
+import { admin, NewAdminEntry } from "../types";
+import bcrypt from 'bcrypt';
+import { parseString } from "../typeGuards";
+/**
+ * 
+ * @description holds crud operations for the admin entity 
+ */
+
+
+/**
+ * creates admin
+ * @param args admin
+ * @returns {admin} created Admin
+ */
+ 
+export const createAdmin = async (args: NewAdminEntry): Promise<admin> => {
+    const saltRounds  = 10;
+    
+    /**
+     * hash password using bycrypt
+     * use typeguards to ensure return value is a string
+     **/
+
+    //eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    const passwordHash : string= parseString(await bcrypt.hash(args.password, saltRounds));
+    const admin = new Admin({...args,password: passwordHash});
+    try {
+        await admin.save();
+    } catch (error) {
+        throw new UserInputError(error.message, {
+            invalidArgs: args,
+        });
+    }
+
+    return admin;
+};
+
+/**
+ * updates admin
+ * @param args admin
+ * @returns {admin} updated Admin
+ */
+
+export const updateAdmin = async (args: admin): Promise<admin> => {
+    const admin = await Admin.findOne({ email: args.email });
+
+    if (!admin) {
+        throw new UserInputError("User is not available", { invalidArgs: args });
+    }
+    admin.clinicsLaunched = args.clinicsLaunched;
+
+    try {
+        await admin.save();
+    } catch (error) {
+        throw new UserInputError(error.message, { invalidArgs: args });
+    }
+    return admin;
+};
+
+
+/**
+ * gets all admins
+ * @returns {admin[]} list of all admins
+ */
+
+export const getAllAdmins = async (): Promise<admin[]> => {
+    const admins : admin[]= await Admin.find();
+
+    return admins;
+};
+
+/**
+ * gets admin
+ * @param args id
+ * @returns {admin} list of all admins
+ */
+
+export const getAdmin = async(args: { id: string; }): Promise<admin> => {
+
+    const admin = await Admin.findById(args);
+
+    if (!admin) {
+        throw new UserInputError("User is not available", { invalidArgs: args.id });
+    }
+
+    return admin;
+};
+
