b6a6128c78e5950dbf5f6e8d711c45f1e518a3c3
brianwachira <brianwachira7@gmail.com>
2021-08-02 01:50:17 +0300

    [FEATURE] : add consultation schemas and resolvers respectively

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,5 +1,6 @@
 import { AdminSchema } from "./AdminSchema";
 import { ClinicSchema } from "./ClinicSchema";
+import { ConsultationSchema } from "./ConsultationSchema";
 import { DefaultSchema } from "./DefaultSchema";
 import { DispatchOfficerSchema } from "./DispatchOfficerSchema";
 import { GuardianSchema } from "./GuardianSchema";
@@ -10,4 +11,4 @@ import { StaffSchema } from "./StaffSchema";
  * @description holds all schemas
  */
 
-export const schema = [DefaultSchema, AdminSchema, ClinicSchema, DispatchOfficerSchema,StaffSchema, PatientSchema, GuardianSchema, NextOfKinSchema];
+export const schema = [DefaultSchema, AdminSchema, ClinicSchema, DispatchOfficerSchema,StaffSchema, PatientSchema, GuardianSchema, NextOfKinSchema, ConsultationSchema];
