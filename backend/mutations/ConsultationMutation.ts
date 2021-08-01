6bef6a21b8e2c7a04ba1d63841202c99a2dbe9e4
brianwachira <brianwachira7@gmail.com>
2021-08-02 02:46:13 +0300

    [FIX] : add typedocs and rename xontet to context

--- backend/mutations/ConsultationMutation.ts
+++ backend/mutations/ConsultationMutation.ts
@@ -1,10 +1,14 @@
 import { createConsultation, updateConsultation } from "../controllers/ConsultationController";
 import { consultation, NewConsultationEntry } from "../types";
 
+/**
+ * @description holds consultation mutation
+ */
+
 export const ConsultationMutation = {
     createConsultation: {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        resolve: async(_parent: any, args: NewConsultationEntry, _contect: any, _info: any) => {
+        resolve: async(_parent: any, args: NewConsultationEntry, _context: any, _info: any) => {
             return await createConsultation(args);
         }
     },
