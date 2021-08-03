f0bd4fbb3c7b63e445cd767022edf07b1b4cb036
brianwachira <brianwachira7@gmail.com>
2021-08-04 00:02:05 +0300

    [ENHANCEMENT] : refactor createClinic to save existingadmin

--- backend/controllers/ClinicController.ts
+++ backend/controllers/ClinicController.ts
@@ -1,4 +1,5 @@
 import { UserInputError } from "apollo-server";
+import Admin from "../models/Admins";
 import Clinic from "../models/Clinics";
 import { clinic, NewClinicEntry } from "../types";
 
@@ -13,9 +14,19 @@ import { clinic, NewClinicEntry } from "../types";
  */
 
 export const createClinic = async (args: NewClinicEntry): Promise<clinic> => {
-    const clinic = new Clinic(args);
+    const existingadmin  = await Admin.findById(args.createdBy);
+
+    if (!existingadmin) {
+        throw new UserInputError("Clinic not available", { invalidArgs: args });
+    }
+
+    let clinic = new Clinic(args);
     try {
-        await clinic.save();
+        clinic = await clinic.save();
+        
+        existingadmin.clinicsLaunched = existingadmin.clinicsLaunched.concat(clinic._id);
+
+        await existingadmin.save();
     } catch (error) {
         throw new UserInputError(error.message, {
             invalidArgs: args,
@@ -50,7 +61,7 @@ export const updateClinic = async (args: clinic): Promise<clinic> => {
  */
 
 export const getAllClinics = async (): Promise<clinic[]> => {
-    const clinic: clinic[] = await Clinic.find();
+    const clinic: clinic[] = await Clinic.find({}).populate('createdBy');
     return clinic;
 };
 
