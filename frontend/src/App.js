be877294478be6f1fb4cf1bd76600a99b1e2612e
brianwachira <brianwachira7@gmail.com>
2021-08-04 23:37:06 +0300

    [FEATURE] : add browser router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter, Switch, Route, Redirect, useRouteMatch} from "react-router-dom"
+import Signup from "./Pages/Auth/signup/signup"
+import ComingSoon from "./Pages/ComingSoon/ComingSoon"
 
 
 function App() {
   return (
     <div>
-      <p>hello</p>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/auth/signup">
+           <Signup/>
+          </Route>
+          <Route path='/'>
+            <ComingSoon/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
