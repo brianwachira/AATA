4476180a03da0bf5da324a770bb21e34d5f817e2
brianwachira <brianwachira7@gmail.com>
2021-08-04 23:56:04 +0300

    [FIX] : rename class to classname

--- frontend/src/Pages/ComingSoon/ComingSoon.js
+++ frontend/src/Pages/ComingSoon/ComingSoon.js
@@ -10,13 +10,13 @@ const ComingSoon = () => {
                 <div className="row h-100">
                     <div className="col-12 col-md-5 h-100 d-grid">
                         <figure className="d-flex flex-column align-self-center justify-content-center">
-                            <i class="fa fa-medkit  fa-4x icon-custom text-center" aria-hidden="true"></i>
+                            <i className="fa fa-medkit  fa-4x icon-custom text-center" aria-hidden="true"></i>
                             {/* <img src="/Assets/images/aa.jpg" alt="" /> */}
                         </figure>
                         <div className="comingsoon-text">
-                            <h5 className='text-center'>This page is under construction</h5>
-                            <h6 className='text-center mb-5'>We are changing a few aesthetics so that you can have the best analysis experience. We will be back up soon.</h6>
-                            <Link to='/'>
+                            <h5 className='text-center'>This dashboard is under construction</h5>
+                            <h6 className='text-center mb-5'>We are changing a few aesthetics so that you can have the best analysis experience. We will be back soon.</h6>
+                            <Link to='/auth/signup'>
                                 <div className="d-flex justify-content-center">
                                     <button className="btn-signup">Sign Up</button>
                                 </div>
