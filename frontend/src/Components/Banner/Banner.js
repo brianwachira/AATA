770b6ca611760f016046cd3760e09bbe2ca76ce4
brianwachira <brianwachira7@gmail.com>
2021-08-04 23:35:07 +0300

    [FEATURE] : Add banner image component

---
+++ frontend/src/Components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import {React} from "react";
+
+import "./Banner.scss"
+const Banner = () => {
+
+    return (
+        <>
+        <main className="banner-img">
+            <div className="img-overlay"></div>
+        </main>
+        </>
+    );
+}
+export default Banner;
