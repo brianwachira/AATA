b5ef3ce1c7ddda2125a64320d4f7fbe8e80142be
brianwachira <brianwachira7@gmail.com>
2021-08-05 21:29:05 +0300

    [FEATURE] : add login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Switch, Route} from "react-router-dom"
+import Login from "./Pages/Auth/login/login"
 import Signup from "./Pages/Auth/signup/signup"
 import ComingSoon from "./Pages/ComingSoon/ComingSoon"
 
@@ -10,6 +11,9 @@ function App() {
         <Switch>
           <Route path="/auth/signup">
            <Signup/>
+          </Route>
+          <Route path="/auth/login">
+           <Login/>
           </Route>
           <Route path='/'>
             <ComingSoon/>
