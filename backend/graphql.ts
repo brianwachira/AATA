49d359cdb9696b0cd58224a70b031fd6b8240c5b
brianwachira <bw@Eupotechs-MacBook-Air.local>
2021-08-05 10:31:47 +0300

    [FIX]: add server plugin landing page playground

--- backend/graphql.ts
+++ backend/graphql.ts
@@ -1,16 +1,19 @@
 import {ApolloServer } from "apollo-server";
 import { resolvers } from "./resolvers";
 import { schema } from "./schemas";
-
+import { ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core';
 /**
  * @description holds and creates apollo server
  */
 
+
 const apolloServer = new ApolloServer({
     typeDefs: schema,
     resolvers,
-    introspection: true,  
-    playground: true, 
+    introspection: true,
+    plugins:[
+        ApolloServerPluginLandingPageGraphQLPlayground()
+    ]
     
 });
 
