a8c0d23be9d79cef253f8391037c68ef48eb1ea5
brianwachira <brianwachira7@gmail.com>
2021-08-07 20:48:49 +0300

    [FEATURE] : add card container

---
+++ frontend/src/Components/Card/Card.js
@@ -0,0 +1,15 @@
+const Card = (props) => {
+
+    const { title, children } = props
+    return (
+        <>
+            <div className="card">
+                <div className="card-body">
+                    <h6 className="card-title">{title}</h6>
+                    {children}
+                </div>
+            </div>
+        </>
+    )
+}
+export default Card
