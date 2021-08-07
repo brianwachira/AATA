3e3be50467ac0bd42fc8dfaab90b3aea0f7a6407
brianwachira <brianwachira7@gmail.com>
2021-08-07 20:57:25 +0300

    [ENHANCEMENT] : refactor link tags

--- frontend/src/Components/SideBar/sidebar.js
+++ frontend/src/Components/SideBar/sidebar.js
@@ -1,43 +1,43 @@
+import { Link } from 'react-router-dom';
 import './sidebar.scss'
 const SideBar = () => {
 
     return (
         <>
-            <div class="d-flex flex-sm-column flex-row flex-nowrap bg-light align-items-center sticky-top">
-                <a href="/" class="d-block p-3 link-dark text-decoration-none" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
-                    
-                <i className="fa fa-medkit  fs-1 icon-custom text-center" aria-hidden="true"></i>
-                </a>
-                <ul class="nav nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto text-center justify-content-between w-100 px-3 align-items-center">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
-                        <i class="fa fa-hospital-o h2" aria-hidden="true"></i>
-                        </a>
+            <div className="d-flex flex-sm-column flex-row flex-nowrap bg-light align-items-center sticky-top">
+                <Link to="/" className="d-block p-3 link-dark text-decoration-none" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
+                    <i className="fa fa-medkit  fs-1 icon-custom text-center" aria-hidden="true"></i>
+                </Link>
+                <ul className="nav nav-custom nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto text-center justify-content-between justify-content-sm-center w-100 px-2 align-items-center">
+                    <li className="nav-item">
+                        <Link to="/comingsoon" className="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
+                            <i className="fa fa-hospital-o h2 icon-sidebars" aria-hidden="true"></i>
+                        </Link>
                     </li>
                     <li>
-                        <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
-                        <i class="fa fa-line-chart h2" aria-hidden="true"></i>
-                        </a>
+                        <Link to="/analytics" className="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
+                            <i className="fa fa-line-chart h2 icon-sidebars" aria-hidden="true"></i>
+                        </Link>
                     </li>
                     <li>
-                        <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Orders">
-                        <i class="fa fa-cogs h2" aria-hidden="true"></i>
-                        </a>
+                        <Link to="/comingsoon" className="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Orders">
+                            <i className="fa fa-cogs h2 icon-sidebars" aria-hidden="true"></i>
+                        </Link>
                     </li>
                     <li>
-                        <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Products">
-                        <i class="fa fa-commenting-o h2" aria-hidden="true"></i>
-                        </a>
+                        <Link to="/comingsoon" className="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Products">
+                            <i className="fa fa-commenting-o h2 icon-sidebars" aria-hidden="true"></i>
+                        </Link>
                     </li>
                 </ul>
-                <div class="dropdown">
-                    <a href="#" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="fa fa-user h2" aria-hidden="true"></i>
-                    </a>
-                    <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
-                        <li><a class="dropdown-item" href="#">New project...</a></li>
-                        <li><a class="dropdown-item" href="#">Settings</a></li>
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                <div className="dropdown">
+                    <Link to="#" className="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i className="fa fa-user h2 icon-sidebars" aria-hidden="true"></i>
+                    </Link>
+                    <ul className="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
+                        <li><Link className="dropdown-item" to="#">New project...</Link></li>
+                        <li><Link className="dropdown-item" to="#">Settings</Link></li>
+                        <li><Link className="dropdown-item" to="#">Profile</Link></li>
                     </ul>
                 </div>
             </div>
