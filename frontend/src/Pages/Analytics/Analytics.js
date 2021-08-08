1d46fd1bef73c9b79a120e2935fa40260f63e85e
brianwachira <brianwachira7@gmail.com>
2021-08-08 18:09:50 +0300

    [FEATURE] : Add mock data for analytics

--- frontend/src/Pages/Analytics/Analytics.js
+++ frontend/src/Pages/Analytics/Analytics.js
@@ -1,8 +1,8 @@
-import { Link } from 'react-router-dom';
 import InnerSideBar from "../../Components/InnerSideBar/InnerSideBar"
 import Layout from "../../Components/Layout/layout"
 import Card from "../../Components/Card/Card"
 import ButtonDropDown from "../../Components/buttonDropDown"
+import MiniChart from "../../Components/MiniChart";
 const Analytics = () => {
     const issues = [
         {
@@ -84,6 +84,50 @@ const Analytics = () => {
             isSolved: false
         }
     ]
+    const dummydata = [
+        {
+            name: "Page A",
+            uv: 4000,
+            pv: 2400,
+            amt: 2400
+        },
+        {
+            name: "Page B",
+            uv: 3000,
+            pv: 1398,
+            amt: 2210
+        },
+        {
+            name: "Page C",
+            uv: 2000,
+            pv: 9800,
+            amt: 2290
+        },
+        {
+            name: "Page D",
+            uv: 2780,
+            pv: 3908,
+            amt: 2000
+        },
+        {
+            name: "Page E",
+            uv: 1890,
+            pv: 4800,
+            amt: 2181
+        },
+        {
+            name: "Page F",
+            uv: 2390,
+            pv: 3800,
+            amt: 2500
+        },
+        {
+            name: "Page G",
+            uv: 3490,
+            pv: 4300,
+            amt: 2100
+        }
+    ];
     return (
         <>
             <Layout>
@@ -94,14 +138,36 @@ const Analytics = () => {
                             <InnerSideBar />
                         </div>
                         <div className="col d-flex  px-sm-4 px-0  flex-column h-sm-100">
-                            <main className="row overflow-auto">
-
+                            <main className="row  mb-3">
                                 <div className="col">
                                     <Card title="issues">
                                         <div className="row overflow-auto flex-row">
                                             {issues.map((issue) =>
-                                                <ButtonDropDown object={issue} className='w-max-content me-2'/>
+                                                <ButtonDropDown object={issue} className='w-max-content me-2' />
                                             )}
+                                        </div>
+                                    </Card>
+                                </div>
+                            </main>
+                            <main className="row">
+                                <div className="col">
+                                    <Card title="Foot fall">
+                                        <div className="row">
+                                            <MiniChart data={dummydata} label='Patients'/>
+                                        </div>
+                                    </Card>
+                                </div>
+                                <div className="col">
+                                    <Card title="Patient Satisfaction">
+                                        <div className="row">
+                                            <MiniChart data={dummydata} label='NPS'/>
+                                        </div>
+                                    </Card>
+                                </div>
+                                <div className="col">
+                                    <Card title="Revenue">
+                                        <div className="row">
+                                            <MiniChart data={dummydata} label='KENYAN SHILLINGS'/>
                                         </div>
                                     </Card>
                                 </div>
