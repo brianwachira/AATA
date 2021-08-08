5441ae9de32bb55a9ffea0dca6d229e531f51e4e
brianwachira <brianwachira7@gmail.com>
2021-08-08 22:01:53 +0300

    [ENHANCEMENT] : set notification component on root

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
+import { useState } from "react"
 import { BrowserRouter, Switch, Route} from "react-router-dom"
+import Notification from "./Components/Notification/Notification"
 import Analytics from "./Pages/Analytics/Analytics"
 import Login from "./Pages/Auth/login/login"
 import Signup from "./Pages/Auth/signup/signup"
@@ -7,15 +9,19 @@ import Home from "./Pages/Home/Home"
 
 
 function App() {
+  
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [title, setTitle] = useState(null)
   return (
     <div>
       <BrowserRouter>
+        <Notification message={errorMessage} title={title}/>
         <Switch>
           <Route path="/auth/signup">
-           <Signup/>
+           <Signup setTitle={setTitle} setMessage={setErrorMessage}/>
           </Route>
           <Route path="/auth/login">
-           <Login/>
+           <Login setTitle={setTitle} setMessage={setErrorMessage}/>
           </Route>
           <Route path='/comingsoon'>
             <ComingSoon/>
